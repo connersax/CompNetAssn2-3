83f4391cfaa2676f4f375a72aa09510718142d02
Conner Sax <conairisawesome@gmail.com>
2020-10-30 17:11:24 -0400

    send and recieve structure

--- job_creator.py
+++ job_creator.py
@@ -2,7 +2,7 @@
 import socket
 
 
-def server_program():
+def creator_program():
     # get the hostname
     host = socket.gethostname()
     port = 5000  # initiate port no above 1024
@@ -12,21 +12,27 @@ def server_program():
     server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
+    server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
+
+    for i in range(1,3):
         data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        print("\nfrom connected user: " + str(data))
+        conn.send("recv".encode())
+
+    # while True:
+    #     # receive data stream. it won't accept data packet greater than 1024 bytes
+    #     data = conn.recv(1024).decode()
+    #     if not data:
+    #         # if data is not received break
+    #         break
+    #     print("from connected user: " + str(data))
+    #     data = input(' -> ')
+    #     conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
 
 if __name__ == '__main__':
-    server_program()
+    creator_program()
