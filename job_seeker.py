707a44d2848885cb29b8f8a728f37b59414bd035
Conner Sax <conairisawesome@gmail.com>
2020-10-30 22:09:54 -0400

    fixed crashing and works better

--- job_seeker.py
+++ job_seeker.py
@@ -29,37 +29,40 @@ def seeker_program():
         data = int.from_bytes(client_socket.recv(1), "big")    # receive job or no job available
         if data == 1:
             print("job_creator: I have corresponding job " + str(int.from_bytes(service, "big")) )
+
+    # send back accept or deny response
+            print("job_seeker: I accept " + str(int.from_bytes(service, "big")) + " job")
+            client_socket.send(bytes([1]))
+
+    # RECEIVE JOB DATA
+            data = client_socket.recv(1024).decode()
+            job_data = data # this would later be saved and used
+            print("job_creator: Job data sent\n")
+
+            client_socket.close()  # close the connection
+
+            time.sleep(2) # job data is processing
+
+            # reconnecting to give result
+            client_socket = socket.socket()  # instantiate
+            client_socket.connect((host, port))  # connect to the server
+
+            print("job_seeker: My IP;UID is " + ip_uid)
+            client_socket.send(ip_uid.encode()) # send ip
+
+            print("job_creator: My IP;UID is " + ip_uid + " waiting for return status of job")
+            data = client_socket.recv(1024).decode()    # creator ip
+
+            print("job_seeker: Job completed with code 0")
+            client_socket.send(bytes([0]))
+
+            print("job_seeker: Sending result data\n")
+            client_socket.send("completed".encode()) # sending result
         else:
-            print("job_creator: I do not have corresponding job " + str(int.from_bytes(service, "big")) )
+            print("job_creator: I do not have corresponding job " + str(int.from_bytes(service, "big")))
+            client_socket.close()  # close the connection
 
-# send back accept or deny response
-        print("job_seeker: I accept " + str(int.from_bytes(service, "big")) + " job")
-        client_socket.send(bytes([1]))
 
-# RECEIVE JOB DATA
-        data = client_socket.recv(1024).decode()
-        job_data = data # this would later be saved and used
-        print("job_creator: Job data sent\n")
-
-        client_socket.close()  # close the connection
-
-        time.sleep(2) # job data is processing
-
-        # reconnecting to give result
-        client_socket = socket.socket()  # instantiate
-        client_socket.connect((host, port))  # connect to the server
-
-        print("job_seeker: My IP;UID is " + ip_uid)
-        client_socket.send(ip_uid.encode()) # send ip
-
-        print("job_creator: My IP;UID is " + ip_uid + " waiting for return status of job")
-        data = client_socket.recv(1024).decode()    # creator ip
-
-        print("job_seeker: Job completed with code 0")
-        client_socket.send(bytes([0]))
-
-        print("job_seeker: Sending result data\n")
-        client_socket.send("completed".encode()) # sending result
 
 if __name__ == '__main__':
     seeker_program()
